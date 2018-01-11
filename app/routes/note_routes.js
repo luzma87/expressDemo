59d092c917099944f4e5624076c2081e24ddef8b
luz <luzma_87@yahoo.com>
2018-01-11 08:29:55 -0500

    notes crud

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,10 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = function (app, db) {
+
     app.post('/notes', (req, res) => {
         const note = {text : req.body.text, title : req.body.title};
-        db.collection('notes').insert(note, (err, result) => {
+        db.collection('notes').insertOne(note, (err, result) => {
             if (err) {
                 res.send({'error' : 'An error has occurred'});
             } else {
@@ -9,4 +12,42 @@ module.exports = function (app, db) {
             }
         });
     });
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id' : new ObjectID(id)};
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error' : 'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id' : new ObjectID(id)};
+        const note = {text : req.body.text, title : req.body.title};
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({'error' : 'An error has occurred'});
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id' : new ObjectID(id)};
+        db.collection('notes').removeOne(details, (err, item) => {
+            if (err) {
+                res.send({'error' : 'An error has occurred'});
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
 };
